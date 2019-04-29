35323a08ce4dfaab86c35231e75adb59a90a9f5e
JDario16 <jamonto5@eafit.edu.co>
2019-04-29 03:53:23 -0500

    Indice invertido
    Falta el readme.md. No se entiende que se debe poner

--- Proyecto3.py
+++ Proyecto3.py
@@ -8,19 +8,18 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 nltk.download('stopwords')
 nltk.download('punkt')
 
-#Lectura de archivos
-filelocation = "dbfs:///FileStore/tables/proyecto3/*.csv"
+def limpieza(cleaned):
+  filelocation = "dbfs:///FileStore/tables/proyecto3/*.csv"
 
-df = spark.read.format("csv").option("header","true").load(filelocation)
+  df = spark.read.format("csv").option("header","true").load(filelocation)
 
-#Selecciona las principales columnas para el futuro
-content = df.select("id","title","content").collect()
+  df.select("id","title","content")
 
-cleaned = []#Lista con la estructura <id:string,title:string,content:list<string>>
+  content = df.select("id","title","content").collect()
 
-#PREPARACION DE DATOS
+  cleaned = []
 
-for row in content:
+  for row in content:
     text = row.__getitem__("content")
     type(text)
     fila = []
@@ -28,14 +27,36 @@ for row in content:
     fila.append(str(row.__getitem__("title")))
     cadena = re.sub('[^A-Za-z0-9]+', ' ', str(row.__getitem__("title"))) + ' ' + re.sub('[^A-Za-z0-9]+', ' ', str(row.__getitem__("content")))
     cadena = re.sub(r'\b\w{1,1}\b', '', cadena)
-
+  
     data = cadena
     stopWords = set(stopwords.words('english'))
     words2 = word_tokenize(data)
     wordsFiltered = []
-
+  
     for w in words2:
         if w not in stopWords:
             wordsFiltered.append(w)
     fila.append(wordsFiltered)
     cleaned.append(fila)
+  return cleaned
+    
+
+def indice_invertido(words):
+  search_word = "" #Aca se almacena la palabra a buscar
+  # Obtiene valor del widget dbutils.widgets.get(name='widget_01')
+  inverted = []
+  for y in range(len(words)):
+    freq = words[y][2].count(search_word)
+    if freq > 0:
+      inverted.append([freq, words[y][0], words[y][1]])
+
+  inverted = sorted(inverted, key=lambda x: x[0])
+  inverted.reverse()
+  for i in inverted[:5]:
+    print(i)
+
+#dbutils.widgets.text(name='widget_01', defaultValue='', label = 'Ingrese la palabra ') # Abre widget que permite escribir la palabra a buscar
+words = []
+words = limpieza(words)
+
+indice_invertido(words)
